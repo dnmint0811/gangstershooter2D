c668b3c0e78dc0a2b86c594d81419dd47e7cd56b
dnmint0811 <dn.mint0811@gmail.com>
2025-07-24 23:25:51 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Tự động chuyển sau 5 giây
